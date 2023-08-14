a86886a7dd741309e3a0fd17595a98d091d57634
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-08-15 01:59:57 +0200

    Added particle effects, player controls

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -6,8 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] InputAction _movement;
+    [SerializeField] float _controlSpeed = 15f;
+    [SerializeField] float _xRange = 5f;
+    [SerializeField] float _yRange = 5f;
+    [SerializeField] float _positionPitchFactor = -4f;
+    [SerializeField] float _controlPitchFactor = -2.5f;
+    [SerializeField] float _positionYawFactor = -1f;
+    [SerializeField] float _controlRollFactor = -2.5f;
+
+    float xThrow;
+    float yThrow;
 
     void Update()
+    {
+        
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
     {
         //Old System
         //float horizontalThrow = Input.GetAxis("Horizontal");
@@ -15,19 +32,35 @@ public class PlayerController : MonoBehaviour
         //float verticalThrow = Input.GetAxis("Vertical");
 
         //New System
-        float xThrow = _movement.ReadValue<Vector2>().x;
-        float yThrow = _movement.ReadValue<Vector2>().y;
+        
+        xThrow = _movement.ReadValue<Vector2>().x;
+        yThrow = _movement.ReadValue<Vector2>().y;
 
-        float xOffset = 0.02f;
-        float yOffset = 0.02f;
+        float xOffset = xThrow * Time.deltaTime * _controlSpeed;
+        float yOffset = yThrow * Time.deltaTime * _controlSpeed;
 
         float xPos = transform.localPosition.x + xOffset;
         float yPos = transform.localPosition.y + yOffset;
 
+        float clampedXPos = Mathf.Clamp(xPos, -_xRange, _xRange);
+        float clampedYPos = Mathf.Clamp(yPos, -_yRange, _yRange);
 
-        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
 
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * _positionPitchFactor;
+        float pitchDueToControlY = yThrow * _controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlY;
 
+        float yawDueToPosition = transform.localPosition.x * _positionYawFactor;
+        float yaw = yawDueToPosition;
+
+        float rollDueToControl = xThrow * _controlRollFactor;
+        float roll = rollDueToControl;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void OnEnable()
